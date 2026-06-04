12822658674e3eaffd86868052de12c014dc338e
MostafaMa9dy <mostafama9dy@gmail.com>
2026-06-04 13:38:25 +0300

    Updates by Mostafa

--- src/Modules/exam/exam.controller.js
+++ src/Modules/exam/exam.controller.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { fileUpload } from "../../Utlis/multer.utlis.js";
 import { validation } from "../../Middelwares/validation.middelwares.js";
+import { authentication } from "../../Middelwares/auth.middlewares.js";
 import { generateExamValidation, generateExamManuallyValidation, publishAIExamValidation } from "./exam.validation.js";
-import { generateExam, uploadPDF, generateExamManually, publishAIExam } from "./exam.service.js";
+import { generateExam, uploadPDF, generateExamManually, publishAIExam, getMyExams } from "./exam.service.js";
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.post("/upload", fileUpload().single("pdfFile"), uploadPDF);
 router.post("/generate", validation(generateExamValidation), generateExam);
 router.post("/generate-manually", validation(generateExamManuallyValidation), generateExamManually);
 router.post("/publish-ai", validation(publishAIExamValidation), publishAIExam);
+router.get("/myExams", authentication, getMyExams);
 
 export default router;
